1d0a042bf58d4e72a6f9318fb88e75679ee51eca
andreavelino02 <andre.ivarras@gmail.com>
2026-07-23 14:21:54 -0400

    feat: add chacara_reservas module to ModuloPreco seed

---
+++ src/Data/Migrations/20260723180958_AddModuloChacaraReservas.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LojaApi.src.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModuloChacaraReservas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "modulos_preco",
+                keyColumn: "id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111101"),
+                column: "atualizado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(4082));
+
+            migrationBuilder.UpdateData(
+                table: "modulos_preco",
+                keyColumn: "id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111102"),
+                column: "atualizado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(4086));
+
+            migrationBuilder.UpdateData(
+                table: "modulos_preco",
+                keyColumn: "id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111103"),
+                column: "atualizado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(4092));
+
+            migrationBuilder.UpdateData(
+                table: "modulos_preco",
+                keyColumn: "id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111104"),
+                column: "atualizado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(4094));
+
+            migrationBuilder.UpdateData(
+                table: "modulos_preco",
+                keyColumn: "id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111105"),
+                column: "atualizado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(4096));
+
+            migrationBuilder.InsertData(
+                table: "modulos_preco",
+                columns: new[] { "id", "atualizado_em", "chave", "disponivel_para_ativar", "nome", "valor" },
+                values: new object[] { new Guid("11111111-1111-1111-1111-111111111106"), new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(4099), "chacara_reservas", true, "Reservas (Chácara/Temporada)", 0m });
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000001"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3033));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000002"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3121));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000003"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3261));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000004"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3455));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000005"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3536));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000006"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3609));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000009"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(2995));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000001"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3675));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000002"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3701));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000003"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3727));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000004"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3751));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000005"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3774));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000006"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3798));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000007"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3821));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000008"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3844));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000009"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(3868));
+
+            migrationBuilder.UpdateData(
+                table: "usuarios",
+                keyColumn: "id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                columns: new[] { "criado_em", "senha_hash" },
+                values: new object[] { new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(2318), "$2a$11$ixxWQLvDXTOhwiDmdczfH.qaufkGEr9gqf1Y4U7Lq1Ck/9E5UAowe" });
+
+            migrationBuilder.UpdateData(
+                table: "usuarios",
+                keyColumn: "id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 18, 9, 57, 459, DateTimeKind.Utc).AddTicks(2892));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "modulos_preco",
+                keyColumn: "id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111106"));
+
+            migrationBuilder.UpdateData(
+                table: "modulos_preco",
+                keyColumn: "id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111101"),
+                column: "atualizado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 572, DateTimeKind.Utc).AddTicks(2095));
+
+            migrationBuilder.UpdateData(
+                table: "modulos_preco",
+                keyColumn: "id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111102"),
+                column: "atualizado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 572, DateTimeKind.Utc).AddTicks(2100));
+
+            migrationBuilder.UpdateData(
+                table: "modulos_preco",
+                keyColumn: "id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111103"),
+                column: "atualizado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 572, DateTimeKind.Utc).AddTicks(2102));
+
+            migrationBuilder.UpdateData(
+                table: "modulos_preco",
+                keyColumn: "id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111104"),
+                column: "atualizado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 572, DateTimeKind.Utc).AddTicks(2105));
+
+            migrationBuilder.UpdateData(
+                table: "modulos_preco",
+                keyColumn: "id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111105"),
+                column: "atualizado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 572, DateTimeKind.Utc).AddTicks(2107));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000001"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 571, DateTimeKind.Utc).AddTicks(7908));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000002"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 571, DateTimeKind.Utc).AddTicks(8246));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000003"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 571, DateTimeKind.Utc).AddTicks(8406));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000004"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 571, DateTimeKind.Utc).AddTicks(8517));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000005"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 571, DateTimeKind.Utc).AddTicks(8600));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000006"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 571, DateTimeKind.Utc).AddTicks(8679));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000009"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 571, DateTimeKind.Utc).AddTicks(7622));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000001"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 571, DateTimeKind.Utc).AddTicks(8802));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000002"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 571, DateTimeKind.Utc).AddTicks(9730));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000003"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 571, DateTimeKind.Utc).AddTicks(9835));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000004"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 571, DateTimeKind.Utc).AddTicks(9863));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000005"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 571, DateTimeKind.Utc).AddTicks(9890));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000006"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 572, DateTimeKind.Utc).AddTicks(1381));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000007"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 572, DateTimeKind.Utc).AddTicks(1483));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000008"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 572, DateTimeKind.Utc).AddTicks(1519));
+
+            migrationBuilder.UpdateData(
+                table: "perfis_loja",
+                keyColumn: "id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000009"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 572, DateTimeKind.Utc).AddTicks(1553));
+
+            migrationBuilder.UpdateData(
+                table: "usuarios",
+                keyColumn: "id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                columns: new[] { "criado_em", "senha_hash" },
+                values: new object[] { new DateTime(2026, 7, 23, 17, 59, 31, 568, DateTimeKind.Utc).AddTicks(7027), "$2a$11$5hBXlFkQVF7L12KKpJwVOujs2oG93yBmeCUo.U2l.nmVaiA5fG7ii" });
+
+            migrationBuilder.UpdateData(
+                table: "usuarios",
+                keyColumn: "id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"),
+                column: "criado_em",
+                value: new DateTime(2026, 7, 23, 17, 59, 31, 568, DateTimeKind.Utc).AddTicks(7660));
+        }
+    }
+}
